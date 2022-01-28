611d2fe8bf1357398b527e4acf2492a2e255cf22
euphoricdesign <noahchukita@gmail.com>
2022-01-28 12:44:15 -0300

    changing some Links for a

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,9 +21,9 @@ const Footer = () => {
                     <div className="wave" id="wave4"></div>
                 </div>
                 <ul className="social-icon">
-                    <li><a href="https://www.facebook.com/profile.php?id=100011077833486v"><i className="fab fa-facebook"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/merlina-villecco-64149a214/"><i className="fab fa-linkedin"></i></a></li>
-                    <li><a href="https://www.instagram.com/merlinavillecco_/"><i className="fab fa-instagram"></i></a></li>
+                    <li><a rel='noreferrer' target="_blank" href="https://www.facebook.com/profile.php?id=100011077833486v"><i className="fab fa-facebook"></i></a></li>
+                    <li><a rel='noreferrer' target="_blank" href="https://www.linkedin.com/in/merlina-villecco-64149a214/"><i className="fab fa-linkedin"></i></a></li>
+                    <li><a rel='noreferrer' target="_blank" href="https://www.instagram.com/merlinavillecco_/"><i className="fab fa-instagram"></i></a></li>
                 </ul>
             </footer>
         </div>
