cc9f8fc38d650794f30786a6cb8b8e9871cb7533
euphoricdesign <noahchukita@gmail.com>
2022-04-15 19:09:34 -0300

    adding third project

--- src/pages/Projects/PortfolioContent.js
+++ src/pages/Projects/PortfolioContent.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react";
 import image from '../../images/Mockup.png';
 import image2 from '../../images/Mockup2.png' ;
+import image3 from '../../images/Mockup3.png' ;
 import gsap from "gsap";
 import "./Projects.scss";
 
@@ -12,10 +13,14 @@ const PortfolioContent = () => {
         const aboutText = document.querySelectorAll('.about-text');
         const secondImage = document.querySelector('.second-image')
         const aboutText2 = document.querySelectorAll('.about-text2');
+        const thirdImage = document.querySelector('.third-image')
+        const aboutText3 = document.querySelectorAll('.about-text3');
         timeline.from(image, {opacity: 0, x: -600, duration: 1.3})
         .from(aboutText, {opacity: 0, x: 600, duration: 1.5, stagger:0.2}, "-=1.3")
         .from(secondImage, {opacity: 0, x: 600, duration: 1.3, stagger:0.2}, "-=1.3")
         .from(aboutText2, {opacity: 0, x: -600, duration: 1.5, stagger:0.2}, "-=1.3")
+        .from(thirdImage, {opacity: 0, x: -600, duration: 1.3, stagger:0.2}, "-=1.3")
+        .from(aboutText3, {opacity: 0, x: 600, duration: 1.5, stagger:0.3}, "-=1.3")
     })
 
 
@@ -55,23 +60,23 @@ const PortfolioContent = () => {
         </div>
       </div>
 
-      {/* <div className="container-work">
+      <div className="container-work">
         <div className="container-img">
-          <img src={image} className="img-size" alt="" />
+          <img src={image3} className="img-size third-image" alt="" />
         </div>
         <div className="container-text">
-          <h4>Red Bull - Torneo Janoz 1X1</h4>
-          <h4 className="tech">Technologies</h4>
-          <ul className="tech-list">
+          <h4 className="about-text3">Congress App</h4>
+          <h4 className="tech about-text3">Technologies</h4>
+          <ul className="tech-list about-text3">
             <i className="fab fa-html5"></i>
             <i className="fab fa-css3-alt"></i>
             <i className="fab fa-js"></i>
             <i className="fab fa-react"></i>
           </ul>
-          <p><a className="button-projects" href="https://redbull-janoz1x1.netlify.app/" target='_blank' rel="noreferrer" title="View site here">View site here</a></p>
+          <p className="about-text3"><a className="button-projects" href="https://congress-app.netlify.app/" target='_blank' rel="noreferrer" title="View site here">View site here</a></p>
         </div>
       </div>
-
+{/* 
       <div className="container-work">
         <div className="container-img">
           <img src={image} className="img-size" alt="" />
