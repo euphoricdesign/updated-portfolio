2232cfd36710233c353665cbb9d33a9de2ede2ac
Merlina <noahchukita@gmail.com>
2024-08-20 22:31:41 -0300

    feat: agrega nuevo proyecto

--- src/pages/Projects/PortfolioContent.js
+++ src/pages/Projects/PortfolioContent.js
@@ -2,7 +2,8 @@ import React, {useEffect} from "react";
 import image from '../../images/mockup4.gif';
 import image2 from '../../images/mockup3.gif' ;
 import image3 from '../../images/mockup2.gif' ;
-import image4 from '../../images/mockup1.gif' ;
+import image4 from '../../images/mockup1.png' ;
+import image5 from '../../images/mockup5.png' ;
 import { RiNextjsFill } from "react-icons/ri";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { BiLogoTypescript } from "react-icons/bi";
@@ -31,15 +32,19 @@ const PortfolioContent = () => {
         const aboutText3 = document.querySelectorAll('.about-text3');
         const fourthImage = document.querySelector('.fourth-image');
         const aboutText4 = document.querySelectorAll('.about-text4');
+        const fifthImage = document.querySelector('.fifth-image')
+        const aboutText5 = document.querySelectorAll('.about-text5');
 
         timeline.from(image, {opacity: 0, x: -600, duration: 1.3})
         .from(aboutText, {opacity: 0, x: 600, duration: 1.5, stagger:0.2}, "-=1.3")
         .from(secondImage, {opacity: 0, x: 600, duration: 1.3, stagger:0.2}, "-=1.3")
         .from(aboutText2, {opacity: 0, x: -600, duration: 1.5, stagger:0.2}, "-=1.3")
-        .from(thirdImage, {opacity: 0, x: -600, duration: 1.3, stagger:0.2}, "-=1.3")
+        .from(thirdImage, {opacity: 0, x: -600, duration: 1.5, stagger:0.3}, "-=1.3")
         .from(aboutText3, {opacity: 0, x: 600, duration: 1.5, stagger:0.3}, "-=1.3")
         .from(fourthImage, {opacity: 0, x: 600, duration: 1.3, stagger:0.4}, "-=1.3")
-        .from(aboutText4, {opacity: 0, x: -600, duration: 1.5, stagger:0.5}, "-=1.3")
+        .from(aboutText4, {opacity: 0, x: -600, duration: 1.4, stagger:0.5}, "-=1.3")
+        .from(fifthImage, {opacity: 0, x: -600, duration: 1.4, stagger:0.5}, "-=1.3")
+        .from(aboutText5, {opacity: 0, x: 600, duration: 1.4, stagger:0.5}, "-=1.3")
     })
 
 
@@ -85,7 +90,7 @@ const PortfolioContent = () => {
         </div>
         <div className="container-text mobile:items-center md:items-start md:pl-[6rem] md:w-[600px] md:h-[600px] flex flex-col justify-center">
           <h4 className="about-text3">DocTrack</h4>
-          <p className="about-text2 text-[#8D8D92] mb-[10px] mobile:text-center md:text-start mobile:text-[14px] md:text-lg">Plataforma de gestión de turnos médicos.</p>
+          <p className="about-text3 text-[#8D8D92] mb-[10px] mobile:text-center md:text-start mobile:text-[14px] md:text-lg">Plataforma de gestión de turnos médicos.</p>
           <h4 className="tech about-text3 font-[500]">Tecnologías</h4>
           <ul className="tech-list about-text3">
             <FaReact className="text-[25px] ml-[6px] text-sky-600" />
@@ -97,17 +102,36 @@ const PortfolioContent = () => {
           <p className="about-text3 w-[150px] h-[100px]"><a className="button-projects" href="https://doc-track-app.vercel.app/" target='_blank' rel="noreferrer" title="View site here">View site here</a></p>
         </div>
       </div>
-      
 
-      <div style={{"marginBottom":"200px"}} className="container-work clase-prueba">
+      <div className="container-work">
         <div className="container-img md:w-[600px] md:h-[600px]">
-          <img src={image4} className="img-size fourth-image" alt="" />
+          <img src={image5} className="img-size fourth-image" alt="" />
         </div>
         <div className="container-text mobile:items-center md:items-start md:pl-[6rem] md:w-[600px] md:h-[600px] flex flex-col justify-center">
-          <h4 className="about-text4">FlickPick</h4>
-          <p className="about-text2 text-[#8D8D92] mb-[10px] mobile:text-center md:text-start mobile:text-[14px] md:text-lg">Plataforma de salas de cine interactivas.</p>
-          <h4 className="tech about-text4 font-[500]">Tecnologías</h4>
+          <h4 className="about-text4">Cal.</h4>
+          <p className="about-text4 text-[#8D8D92] mb-[10px] mobile:text-center md:text-start mobile:text-[14px] md:text-lg">Organizador de eventos.</p>
+          <h4 className="about-text4 tech font-[500]">Tecnologías</h4>
           <ul className="tech-list about-text4">
+            <FaReact className="text-[25px] ml-[6px] text-sky-600" />
+            <RiTailwindCssFill className="text-[25px] ml-[6px] text-sky-500" />
+            <SiExpress className="text-[25px] ml-[6px] text-gray-700" />
+            <FaNodeJs className="text-[25px] ml-[6px] text-green-700" />
+            <SiPostgresql className="text-[25px] ml-[6px] text-sky-800" />
+          </ul>
+          <p className="about-text4 w-[150px] h-[100px]"><a className="button-projects" href="https://eventorganizerapp.netlify.app/" target='_blank' rel="noreferrer" title="View site here">View site here</a></p>
+        </div>
+      </div>
+      
+
+      <div style={{"marginBottom":"200px"}} className="container-work">
+        <div className="container-img md:w-[600px] md:h-[600px]">
+          <img src={image4} className="img-size fifth-image" alt="" />
+        </div>
+        <div className="container-text mobile:items-center md:items-start md:pl-[6rem] md:w-[600px] md:h-[600px] flex flex-col justify-center">
+          <h4 className="about-text5">FlickPick</h4>
+          <p className="about-text5 text-[#8D8D92] mb-[10px] mobile:text-center md:text-start mobile:text-[14px] md:text-lg">Plataforma de salas de cine interactivas.</p>
+          <h4 className="tech about-text5 font-[500]">Tecnologías</h4>
+          <ul className="tech-list about-text5">
             <FaHtml5 className="text-[25px] ml-[6px] text-orange-500" />
             <FaCss3Alt className="text-[25px] ml-[6px] text-sky-600" />
             <IoLogoJavascript className="text-[25px] ml-[6px] text-yellow-400" />
@@ -115,7 +139,7 @@ const PortfolioContent = () => {
             <SiExpress className="text-[25px] ml-[6px] text-gray-700" />
             <DiMongodb className="text-[25px] ml-[6px] text-green-400" />
           </ul>
-          <p className="about-text4 w-[150px] h-[100px]"><a className="button-projects" href="https://flickpickapp.netlify.app/" target='_blank' rel="noreferrer" title="View site here">View site here</a></p>
+          <p className="about-text5 w-[150px] h-[100px]"><a className="button-projects" href="https://flickpickapp.netlify.app/" target='_blank' rel="noreferrer" title="View site here">View site here</a></p>
         </div>
       </div>
 {/* 
